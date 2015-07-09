86a7d765f89bb3d368ff11c0d7d054845f26e37d
Daisuke Hirata <daisukihirata@gmail.com>
2015-07-09 18:23:26 +0900

    first commit

---
+++ PhotoPicker/APLPhotoCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  APLPhotoCollectionViewController.h
+//  PhotoPicker
+//
+//  Created by Daisuke Hirata on 2015/07/09.
+//  Copyright (c) 2015年 Apple Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface APLPhotoCollectionViewController : UICollectionViewController
+@property (nonatomic) NSArray *capturedImages;
+@end
